346258383fd70cd7ca8918c87f4d879549b55981
naomi-rc <naomi.cat182128@gmail.com>
2018-11-25 01:24:21 -0500

    commit to save

--- app/models/Tutor.php
+++ app/models/Tutor.php
@@ -46,44 +46,31 @@ class Tutor extends Model
 		$programSql = "";
 		$priceSql = "";
 
-		$array = [];
+		$execute_array = [];
 
 		if($subject != ''){
 			$subjectSql = " AND t.description LIKE :subject";
 			$subject = "%$subject%";
-			$array['subject'] = $subject;
-//			array_push($array, "subject", $subject);
+			$execute_array['subject'] = $subject;
 		}
 
 		if($programId != '') {
-			$programSql = " AND p.programId = :programId";
-			array_push($array, "programId", $programId);
+			$programSql = " AND p.programId = :programId";		
+			$execute_array['programId'] = $programId;
 		}
 
 		if($price_lower != 0 && $price_upper != 0){
 			$priceSql = " AND t.pay BETWEEN :price_lower AND :price_upper";
-			array_push($array, "price_lower", $price_lower);
-			array_push($array, "price_upper", $price_upper);
-		}
-
-//		$whereClause = implode(" AND ", $array);
-
-		/*$sql = "SELECT t.userId, p.firstName, p.lastName, p.profileImagePath, t.pay, t.description, t.timesTutored, t.rating FROM Tutor t, Profile p 
-				WHERE t.userId=p.userId AND t.description LIKE :subject AND (p.programId = :programId OR t.pay BETWEEN :price_lower AND :price_upper)";*/
-
-
-//				WHERE t.userId=p.userId AND t.description LIKE :subject AND (p.programId = :programId OR t.pay BETWEEN :price_lower AND :price_upper)";*/
-		
-
+			$execute_array['price_lower'] = $price_lower;
+			$execute_array['price_upper'] = $price_upper;
+		}	
 
 		$sql = "SELECT t.userId, p.firstName, p.lastName, p.profileImagePath, t.pay, t.description, t.timesTutored, t.rating FROM Tutor t, Profile p 
 				WHERE t.userId=p.userId".$subjectSql.$programSql.$priceSql;
 
-		echo $sql;
-		var_dump($array);
 		$stmt = self::$_connection->prepare($sql);
-    	//$stmt->execute(['subject'=>$subject, 'programId'=>$programId, 'price_lower'=>$price_lower, 'price_upper'=>$price_upper]);
-		$stmt->execute($array);
+    
+		$stmt->execute($execute_array);
 		
         $stmt->setFetchMode(PDO::FETCH_CLASS, "Tutor");
 		return $stmt->fetchAll();
