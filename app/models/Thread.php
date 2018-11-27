383f284a7e2ac3f918109e04b316642f17ca0145
cstuser <cstuser>
2018-11-27 10:57:53 -0500

    redid threads

--- app/models/Thread.php
+++ app/models/Thread.php
@@ -9,16 +9,27 @@ class Thread extends Model {
 	}
 
 	public function getUserThreads($userId){
-		$sql = "SELECT * FROM thread t, profile p
+		/*$sql = "SELECT * FROM thread t, profile p
 		WHERE p.userId = t.firstUserId || p.userId = t.secondUserId AND
-		(firstUserId = :userId || secondUserId = :secondUserId)";
+		(firstUserId = :userId || secondUserId = :secondUserId)";*/
+
+		$sql = "SELECT * FROM profile p, thread t
+				WHERE (p.userId = t.firstUserId OR p.userId = t.secondUserId)
+				AND (t.firstUserId = :userId OR t.secondUserId = :userId)
+				AND p.userid != :userId;";
+
 		$stmt = self::$_connection->prepare($sql);
-		$stmt->execute(['firstUserId'=>$this->firstUserId, 'secondUserId'=>$this->secondUserId]);
+		$stmt->execute(['userId'=>$userId]);
 		$stmt->setFetchMode(PDO::FETCH_CLASS, "Thread");
 		return $stmt->fetchAll();
 	}
 
 }
-
+/*
+select * from profile p, thread t
+where (p.userId = t.firstUserId OR p.userId = t.secondUserId)
+and (t.firstUserId = 2 OR t.secondUserId = 2)
+and p.userid != 2;
+*/
 
 ?>
--- app/views/Thread/index.php
+++ app/views/Thread/index.php
@@ -14,19 +14,38 @@
 
 			<div class="content_block">				
 				<?php
-				if(isset($data['message'])){
+					if(isset($data['message'])){
 
-					$message = $data['message'];
-				
-					if($message != ''){
-						print("<div class='alert alert-success' role='alert'><strong>$message</strong></div>");
+						$message = $data['message'];
+					
+						if($message != ''){
+							print("<div class='alert alert-success' role='alert'><strong>$message</strong></div>");
+						}
 					}
-				}
 				?>
 				<div>
 					<h4>Threads</h4>	
-					
+					<div class="">
+						<?php 
+							$threads = $data['threads'];
 
+							foreach($threads as $thread){											
+								
+								print("
+									<a class='row' href='/Thread/detail/$thread->threadId'>
+										<div class='col-md-1'>
+											<img src='/images/$thread->profileImagePath' width='75%' style='border-radius: 25%'/>
+										</div>
+										<div class='col-md-2'>
+										  <h5 class='card-header'>$thread->firstName $thread->lastName</h5>
+										  <div class='card-body'>								    
+										    <p class='card-text'></p>										    
+										  </div>
+										</div>
+									</a><hr/>");
+								}
+						?>
+					</div>
 				</div>
 
 			</div>
