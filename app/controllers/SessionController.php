f5094306f1003cfb5c4a893ea51fcea38a90c0b3
naomi-rc <naomi.cat182128@gmail.com>
2018-12-02 18:27:03 -0500

    updated php include for all css/bootstrap links

---
+++ app/controllers/SessionController.php
@@ -0,0 +1,9 @@
+<?php
+class SessionController extends Controller {
+
+	public function index() {
+		
+	}
+}
+
+?>
--- app/views/Tutor/create.php
+++ app/views/Tutor/create.php
@@ -1,18 +1,7 @@
 <html>
 	<head>
 
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">		
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-		<link rel="stylesheet" href="../../../css/default_styles.css" type="text/css" />
-		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
-		<script src="../../../javascript/script.js"></script>
-
-		<link href='https://cdn.jsdelivr.net/npm/froala-editor@2.9.1/css/froala_editor.min.css' rel='stylesheet' type='text/css' />
-		<link href='https://cdn.jsdelivr.net/npm/froala-editor@2.9.1/css/froala_style.min.css' rel='stylesheet' type='text/css' />
- 
-		<!-- Include JS file. -->
-		<script type='text/javascript' src='https://cdn.jsdelivr.net/npm/froala-editor@2.9.1/js/froala_editor.min.js'></script>
+		<?php include_once('app/views/Default/stylesheet_links.php') ?>
 	</head>
 	<body>
 		<?php include_once('app/views/Default/header.php') ?>
